9b33a5616cae7818193bd8333c8be5180ea0099c
Adam Yuuki Oyama <adamyuuki@weg.net>
2021-05-13 12:33:31 -0300

    mobile started

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 shareBtn = $("#share");
 cardSocialClass = $(".card-social");
+shareBg = $(".share-bg");
+arrow = $("#arrow");
 
 controler = false;
 
@@ -8,8 +10,14 @@ shareBtn.click(handleClick);
 function handleClick(){
   if(controler){
     cardSocialClass.css("display", "none");
+    shareBg.css("background-color", "hsl(210, 46%, 95%)");
+    shareBtn.css("background-color", "hsl(210, 46%, 95%)");
+    arrow.css("color", "hsl(217, 19%, 35%)");
   } else {
     cardSocialClass.css("display", "flex");
+    shareBtn.css("background-color", "hsl(217, 19%, 35%)");
+    shareBg.css("background-color", "hsl(217, 19%, 35%)");
+    arrow.css("color", "hsl(210, 46%, 95%)");
   }
 
   controler = !controler;
