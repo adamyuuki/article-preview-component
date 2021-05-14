7f803dbf3bf773b103ff736c49c7dda5f46ead01
Adam Yuuki Oyama <adamyuuki@weg.net>
2021-05-14 12:30:28 -0300

    14.05

--- app.js
+++ app.js
@@ -2,22 +2,44 @@ shareBtn = $("#share");
 cardSocialClass = $(".card-social");
 shareBg = $(".share-bg");
 arrow = $("#arrow");
+winWidth = $(window).width();
+cardBottomClass = $(".card-bottom");
 
 controler = false;
 
 shareBtn.click(handleClick);
 
 function handleClick(){
-  if(controler){
-    cardSocialClass.css("display", "none");
-    shareBg.css("background-color", "hsl(210, 46%, 95%)");
-    shareBtn.css("background-color", "hsl(210, 46%, 95%)");
-    arrow.css("color", "hsl(217, 19%, 35%)");
-  } else {
-    cardSocialClass.css("display", "flex");
-    shareBtn.css("background-color", "hsl(217, 19%, 35%)");
-    shareBg.css("background-color", "hsl(217, 19%, 35%)");
-    arrow.css("color", "hsl(210, 46%, 95%)");
+
+  if (winWidth<576){
+
+    if(controler){
+
+      cardBottomClass.css("display", "flex");
+      cardSocialClass.css("display", "none");
+      shareBg.css("background-color", "hsl(210, 46%, 95%)");
+      shareBtn.css("background-color", "hsl(210, 46%, 95%)");
+      arrow.css("color", "hsl(217, 19%, 35%)");
+    } else {
+      cardBottomClass.css("display", "none");
+      cardSocialClass.css("display", "flex");
+      shareBtn.css("background-color", "hsl(217, 19%, 35%)");
+      shareBg.css("background-color", "hsl(217, 19%, 35%)");
+      arrow.css("color", "hsl(210, 46%, 95%)");
+    }
+
+  } else{
+    if(controler){
+      cardSocialClass.css("display", "none");
+      shareBg.css("background-color", "hsl(210, 46%, 95%)");
+      shareBtn.css("background-color", "hsl(210, 46%, 95%)");
+      arrow.css("color", "hsl(217, 19%, 35%)");
+    } else {
+      cardSocialClass.css("display", "flex");
+      shareBtn.css("background-color", "hsl(217, 19%, 35%)");
+      shareBg.css("background-color", "hsl(217, 19%, 35%)");
+      arrow.css("color", "hsl(210, 46%, 95%)");
+    }
   }
 
   controler = !controler;
